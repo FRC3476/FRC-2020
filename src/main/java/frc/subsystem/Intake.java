3ae6e2ccc2be94610ca0199da5073785e8195b27
varun7654 <59785640+varun7654@users.noreply.github.com>
2020-02-02 15:49:06 -0800

    updated git ignore

--- src/main/java/frc/subsystem/Intake.java
+++ src/main/java/frc/subsystem/Intake.java
@@ -1,30 +1,30 @@
-package frc.subsystem;
+// package frc.subsystem;
 
-import frc.robot.Constants;
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import frc.utility.telemetry.TelemetryServer;
-import frc.utility.LazyTalonSRX;
-import frc.utility.LazySparkSRX;
-import edu.wpi.first.wpilibj.Solenoid;
+// import frc.robot.Constants;
+// import com.ctre.phoenix.motorcontrol.ControlMode;
+// import frc.utility.telemetry.TelemetryServer;
+// import frc.utility.LazyTalonSRX;
+// import frc.utility.LazySparkSRX;
+// import edu.wpi.first.wpilibj.Solenoid;
 
-public class Intake extends Subsystem {
+// public class Intake extends Subsystem {
 
-    public enum DeployState {
-            DEPLOY, UNDEPLOY, DEPLOYING
-    }
-    public enum IntakeState {
-            ACTIVATED, UNACTIVATED, RELEASE
-    }
+//     public enum DeployState {
+//             DEPLOY, UNDEPLOY, DEPLOYING
+//     }
+//     public enum IntakeState {
+//             ACTIVATED, UNACTIVATED, RELEASE
+//     }
 
-instance = new Intake();
+// instance = new Intake();
 
-private TelemetryServer telemetryServer = TelemetryServer.getInstance();
-private Solenoid deploySolenoid;
-private LazyTalonSRX intakeMotor;
-private LazySparkSRX intakeMotor;
-private DeployState deployState = DeployState.UNDEPLOY;
-private DeployState deployState = DeployState.UNACTIVATED;
+// private TelemetryServer telemetryServer = TelemetryServer.getInstance();
+// private Solenoid deploySolenoid;
+// private LazyTalonSRX intakeMotor;
+// private LazySparkSRX intakeMotor;
+// private DeployState deployState = DeployState.UNDEPLOY;
+// private DeployState deployState = DeployState.UNACTIVATED;
 
-private double lastDeployCommandTime;
+// private double lastDeployCommandTime;
 
-private Intake() {
+// private Intake() {
