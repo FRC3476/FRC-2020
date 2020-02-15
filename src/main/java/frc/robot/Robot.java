c29731b37b21c22409cbf6f2c518db132d7edbb1
varun7654 <59785640+varun7654@users.noreply.github.com>
2020-02-15 15:24:32 -0800

    pathing auto test

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -156,7 +156,7 @@ public class Robot extends TimedRobot {
     else startPos = 48+18;
 
    // if(m_chooser.getSelected().equals("Cargo 1_2")&& red_blue.getSelected().equals("Red")) option = new Ship1_2Red(autoDir, startPos);
-  
+    option = new PathingTest(1, 0);
     
     auto = new Thread(option);
     auto.start();
