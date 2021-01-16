4761dc716919e2e36346bd88efabf5ec9cc996df
misu13 <71414271+misu13@users.noreply.github.com>
2021-01-16 15:58:25 -0800

    Added file
    Added the autonav bounce file, and added some drive stuff. idk if it's correct... basically just copied the example that Varun did last week.

---
+++ src/main/java/frc/auton/AutonavBouncePath.java
@@ -0,0 +1,52 @@
+package frc.auton;
+
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.Constants;
+import frc.subsystem.*;
+import frc.utility.math.*;
+import frc.utility.control.motion.Path;
+
+import frc.subsystem.Intake;
+import frc.subsystem.Intake.DeployState;
+import frc.subsystem.Intake.IntakeState;
+import frc.subsystem.Shooter.ShooterState;
+import frc.subsystem.VisionManager.VisionStatus;
+
+
+@SuppressWarnings("unused")
+public class AutonavBouncePath extends TemplateAuto implements Runnable  {
+	Drive drive = Drive.getInstance();
+	RobotTracker robotTracker = RobotTracker.getInstance();
+	Intake intake = Intake.getInstance();
+	VisionManager vision = VisionManager.getInstance();
+	Shooter shooter = Shooter.getInstance();
+
+	private double TargetTime;
+	
+	boolean killSwitch = false;
+
+
+	public AutonavBouncePath(double startY) {
+		//RobotTracker.getInstance().setInitialTranslation(new Translation2D(startX, 75));
+		super(new Translation2D(120.5, startY));
+		robotTracker.setInitialRotation(Rotation2D.fromDegrees(180));
+	}
+
+	@Override
+	public void run() {
+
+		//Start 75 120
+		System.out.println("Only Shoot");
+		double turnAngle = 0;
+		
+		//turnAngle = Math.toDegrees(Math.atan2(75 , here().getX()-48));
+		Translation2D target = new Translation2D(0, 67);
+		Translation2D robot = here();
+
+		Rotation2D pointAtTarget = robot.getAngle(target);
+		System.out.println(target);
+
+		
+	}
+
+}
