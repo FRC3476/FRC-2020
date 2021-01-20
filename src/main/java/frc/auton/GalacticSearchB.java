5cf3480c2395a4338c2800e9bd7ca29e77d36193
Micah <hsu1310@gmail.com>
2021-01-20 11:51:15 -0800

    Update GalacticSearchB.java

--- src/main/java/frc/auton/GalacticSearchB.java
+++ src/main/java/frc/auton/GalacticSearchB.java
@@ -1,5 +1,7 @@
 package frc.auton;
 
+import java.util.ArrayList;
+
 import edu.wpi.first.wpilibj.Timer;
 import frc.robot.Constants;
 import frc.subsystem.*;
@@ -12,9 +14,8 @@ import frc.subsystem.Intake.IntakeState;
 import frc.subsystem.Shooter.ShooterState;
 import frc.subsystem.VisionManager.VisionStatus;
 
-
 @SuppressWarnings("unused")
-public class GalacticSearchB extends TemplateAuto implements Runnable  {
+public class GalacticSearchA extends TemplateAuto implements Runnable  {
 	Drive drive = Drive.getInstance();
 	RobotTracker robotTracker = RobotTracker.getInstance();
 	Intake intake = Intake.getInstance();
@@ -25,16 +26,63 @@ public class GalacticSearchB extends TemplateAuto implements Runnable  {
 	
 	boolean killSwitch = false;
 
+    private int startY;
+    private boolean pathIsRed;
 
-	public GalacticSearchB() {
+    public void turnOnIntakeTrack() {
+		intake.setDeployState(Intake.DeployState.DEPLOY);
+		intake.setSpeed(Constants.IntakeMotorPower);
+		hopper.setFrontMotorState(Hopper.FrontMotorState.ACTIVE);
+		hopper.setSnailMotorState(Hopper.SnailMotorState.ACTIVE, false);
+	}
+
+	public void turnOffIntakeTrack() {
+		intake.setSpeed(0);
+		hopper.setFrontMotorState(Hopper.FrontMotorState.INACTIVE);
+		hopper.setSnailMotorState(Hopper.SnailMotorState.INACTIVE, false);
+	}
+    
+	public GalacticSearchA() {
         //RobotTracker.getInstance().setInitialTranslation(new Translation2D(startX, 75));
-		super(new Translation2D(0, 0));
-		robotTracker.setInitialRotation(Rotation2D.fromDegrees(180));
+        super(new Translation2D(0, 0));
+        
+        // need some vision stuff here
+        startY = pathIsRed ? 120 : 60;
+		RobotTracker.getInstance().setInitialTranslation(new Translation2D(46, startY));
+        robotTracker.setInitialRotation(Rotation2D.fromDegrees(180));
 	}
 
 	@Override
 	public void run() {
+        ArrayList<Translation2D> points = new ArrayList<Translation2D>();
+        if(pathIsRed) {
+            points.add(new Translation2D(90, 120));
+            points.add(new Translation2D(120, 60));
+            points.add(new Translation2D(210, 120));
+            points.add(new Translation2D(314, 120));
+        }else {
+            points.add(new Translation2D(180, 60));
+            points.add(new Translation2D(240, 120));
+            points.add(new Translation2D(300, 60));
+            points.add(new Translation2D(314, 60));
+        }
         
+        turnOnIntakeTrack();
+
+        Path p1 = new Path(here());
+        drive.setAutoPath(p1, false);
+        
+        while(!drive.isFinished()) {
+            if(isDead()) {
+                return;
+            }
+        }
+
+        turnOffIntakeTrack();
+
+		synchronized (this) {
+			done = true; 
+		}
 	}
 
 }
