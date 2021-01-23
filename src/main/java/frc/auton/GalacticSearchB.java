0533bd649f9a8e254a43bb1da5959dce3702da15
Aditya Rawat <aditya1rawat@gmail.com>
2021-01-23 15:55:58 -0800

    added basic path

---
+++ src/main/java/frc/auton/GalacticSearchB.java
@@ -0,0 +1,42 @@
+package frc.auton;
+
+import frc.subsystem.*;
+import frc.utility.math.*;
+import frc.utility.control.motion.Path;
+
+import frc.subsystem.Intake;
+
+public class GalacticSearchB extends TemplateAuto {
+    Drive drive = Drive.getInstance();
+    RobotTracker robotTracker = RobotTracker.getInstance();
+    Intake intake = Intake.getInstance();
+    VisionManager vision = VisionManager.getInstance();
+    Shooter shooter = Shooter.getInstance();
+
+    boolean killSwitch = false;
+
+    public GalacticSearchB() {
+        super(new Translation2D(15, 60));
+        robotTracker.setInitialRotation(Rotation2D.fromDegrees(180));
+        
+        Path bluePath = new Path(here());
+
+        bluePath.addPoint(new Translation2D(180,60), 30);
+        bluePath.addPoint(new Translation2D(240,120), 30);
+        bluePath.addPoint(new Translation2D(300,60), 30);
+        bluePath.addPoint(new Translation2D(345,90), 30);
+
+        drive.setAutoPath(bluePath, false);
+
+        while(!drive.isFinished()) {
+            if(isDead()) {
+                return;
+            }
+        }
+
+		synchronized (this) {
+			done = true; 
+		}
+
+    }
+}
