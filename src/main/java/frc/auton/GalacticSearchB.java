c6a80f7bdb5adc43fb092ff4f5ef741814da85cc
Aditya Rawat <aditya1rawat@gmail.com>
2021-02-13 15:56:21 -0800

    path set

--- src/main/java/frc/auton/GalacticSearchB.java
+++ src/main/java/frc/auton/GalacticSearchB.java
@@ -30,33 +30,36 @@ public class GalacticSearchB extends TemplateAuto {
 	}
 
     public void run(){
-        Path bluePath = new Path(here());
-        Path redPath = new Path(here());
-
-        // Will default to Blue Path
-        Boolean isBlue = true;
-
-        //Blue Path Points
-        bluePath.addPoint(new Translation2D(180,60), 50);
-        bluePath.addPoint(new Translation2D(240,120), 50);
-        bluePath.addPoint(new Translation2D(300,60), 50);
-        bluePath.addPoint(new Translation2D(314,30), 50);
         
-        //Red Path Points
-        redPath.addPoint(new Translation2D(150,60), 50);
-        redPath.addPoint(new Translation2D(210,120), 50);
-        // redPath.addPoint(new Translation2D(300,60), 30);
-        redPath.addPoint(new Translation2D(345,120), 50);
+        
+        Boolean isBlue;
+        
+        
+        
+        
 
         // Vision Stuff To Decide Which Will Decide Which Path, And Set var isBlue accordingly (Is manual right now)
 
         if(Limelight.getInstance().isTargetVisiable()) {
             System.out.println("Blue Path");
             RobotTracker.getInstance().setInitialTranslation(new Translation2D(45, 60));
+            Path bluePath = new Path(here());
+            //Blue Path Points
+            bluePath.addPoint(new Translation2D(180,60), 50);
+            bluePath.addPoint(new Translation2D(240,120), 50);
+            bluePath.addPoint(new Translation2D(300,60), 50);
+            bluePath.addPoint(new Translation2D(314,30), 50);
+            isBlue = true;
             drive.setAutoPath(bluePath, true);
         } else {
             System.out.println("Red Path");
             RobotTracker.getInstance().setInitialTranslation(new Translation2D(45, 120));
+            Path redPath = new Path(here());
+            //Red Path Points
+            redPath.addPoint(new Translation2D(150,60), 50);
+            redPath.addPoint(new Translation2D(210,120), 50);
+            // redPath.addPoint(new Translation2D(300,60), 30);
+            redPath.addPoint(new Translation2D(345,120), 50);
             isBlue = false;
             drive.setAutoPath(redPath, true);
         }
@@ -72,7 +75,6 @@ public class GalacticSearchB extends TemplateAuto {
                 return;
             }
             if(isBlue) {
-
                 if(bluePath.getPercentage() >= 90){
                     intake.setDeployState(Intake.DeployState.UNDEPLOY);
                     intake.setSpeed(0);
