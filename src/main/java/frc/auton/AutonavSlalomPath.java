5d34cdee9ddbd0067930c9c074d5e4f65127e364
Aditya Rawat <aditya1rawat@gmail.com>
2021-02-13 17:22:25 -0800

    Galactic search b (#34)
    * Autonav Slalom Path
    
    Added files
    
    * Update AutonavSlalomPath.java
    
    * Update AutonavSlalomPath.java
    
    * Update AutonavSlalomPath.java
    
    * Updated to 2021
    
    * Changed ArrayList to Map
    
    * Update AutonavSlalomPath.java
    
    * Made the end a bit closer to the marker
    
    * set auto to the salalom path
    
    * changed some speeds + points
    
    * changed speeds + points
    
    * normalized speeds
    
    * more changes
    
    * Update AutonavSlalomPath.java
    
    * Update AutonavSlalomPath.java
    
    * Update AutonavSlalomPath.java
    
    * Update AutonavSlalomPath.java
    
    * added basic path
    
    * Update AutonavSlalomPath.java
    
    * Update AutonavSlalomPath.java
    
    * fixed run function
    
    * other stuff after build
    
    * Update AutonavSlalomPath.java
    
    * changed location of moving to the endzone
    
    * set auto to use the slalom path
    
    * changed start position
    
    * Update AutonavSlalomPath.java
    
    * add .gitignore
    
    * backwards time
    
    * set auto to galactic serch b
    
    * added gradle to gitignore and built
    
    * built and added gitignore
    
    * turned on intake
    
    * added constants file
    
    * imported constants
    
    * added intake control over the whoel course
    
    * reduced percentage for colsing intake
    
    * added red path and some logic for it in the future
    
    * made faster
    
    * added dead statement
    
    * added start point logic
    
    * one more thing
    
    * translation 2d
    
    * added vision
    
    * added redpath
    
    * did command
    
    * fixed thingy
    
    * path set
    
    * path set
    
    * speed change
    
    * made blue path faster + drift compensation
    
    * point
    
    * made red path faster + fixed intake not closing
    
    * Delete .gitignore
    
    * removed option
    
    * Revert "Delete .gitignore"
    
    This reverts commit e88512b50ea885a702a4eeb0a0dceba8aa3aaabf.
    
    Co-authored-by: misu13 <71414271+misu13@users.noreply.github.com>
    Co-authored-by: Micah <hsu1310@gmail.com>
    Co-authored-by: = <=>
    Co-authored-by: varun7654 <59785640+varun7654@users.noreply.github.com>

---
+++ src/main/java/frc/auton/AutonavSlalomPath.java
@@ -0,0 +1,69 @@
+package frc.auton;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.Constants;
+import frc.subsystem.*;
+import frc.utility.math.*;
+import frc.utility.control.motion.Path;
+
+import frc.subsystem.Intake;
+import frc.subsystem.Intake.DeployState;
+import frc.subsystem.Intake.IntakeState;
+import frc.subsystem.Shooter.ShooterState;
+import frc.subsystem.VisionManager.VisionStatus;
+
+@SuppressWarnings("unused")
+public class AutonavSlalomPath extends TemplateAuto implements Runnable  {
+	Drive drive = Drive.getInstance();
+	RobotTracker robotTracker = RobotTracker.getInstance();
+	Intake intake = Intake.getInstance();
+	VisionManager vision = VisionManager.getInstance();
+	Shooter shooter = Shooter.getInstance();
+
+	private double TargetTime;
+	
+	boolean killSwitch = false;
+
+
+	public AutonavSlalomPath() {
+		//RobotTracker.getInstance().setInitialTranslation(new Translation2D(startX, 75));
+		super(new Translation2D(42, 30));
+        robotTracker.setInitialRotation(Rotation2D.fromDegrees(0));
+	}
+
+	@Override
+	public void run() {
+        int speed = 30;
+
+        Path p1 = new Path(here());
+        p1.addPoint(new Translation2D(75, 30), speed);
+        p1.addPoint(new Translation2D(105, 90), speed);
+        p1.addPoint(new Translation2D(260, 90), speed);
+        p1.addPoint(new Translation2D(290, 10), speed);
+        p1.addPoint(new Translation2D(315, 40), speed);
+        p1.addPoint(new Translation2D(315, 60), speed);
+        p1.addPoint(new Translation2D(315, 90), speed);
+        p1.addPoint(new Translation2D(290, 110), speed);
+        p1.addPoint(new Translation2D(260, 30), speed);
+        p1.addPoint(new Translation2D(105, 30), speed);
+        p1.addPoint(new Translation2D(75, 85), speed);
+        p1.addPoint(new Translation2D(42, 85), speed);
+
+        drive.setAutoPath(p1, false);
+
+        while(!drive.isFinished()) {
+            if(isDead()) {
+                return;
+            }
+        }
+
+		synchronized (this) {
+			done = true; 
+		}
+		
+	}
+
+}
