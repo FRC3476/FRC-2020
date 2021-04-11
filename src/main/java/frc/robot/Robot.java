7f46712e16a6e5ab0ecd8e51cb52c01df26f0855
varun7654 <59785640+varun7654@users.noreply.github.com>
2021-04-11 13:09:52 -0700

    remove print

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -232,10 +232,6 @@ public class Robot extends TimedRobot {
 	 */
 	@Override
 	public void autonomousPeriodic() {
-		DecimalFormat df = new DecimalFormat();
-		df.setMaximumFractionDigits(2);
-		System.out.println("robot angle: " + df.format(robotTracker.getOdometry().rotationMat.getDegrees()) + " position: X: " + 
-			df.format(robotTracker.getOdometry().translationMat.getX()) + " Y: " + df.format(robotTracker.getOdometry().translationMat.getY()));
 		/*
 		buttonPanel.update();
 		if(!autoDone) {
