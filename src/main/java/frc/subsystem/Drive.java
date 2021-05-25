913682602552dcbcc1b23cad0d2ea9cb7c137616
varun7654 <59785640+varun7654@users.noreply.github.com>
2021-05-25 11:35:31 -0700

    implement the swerve odometry

--- src/main/java/frc/subsystem/Drive.java
+++ src/main/java/frc/subsystem/Drive.java
@@ -2,7 +2,24 @@
 
 package frc.subsystem;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.revrobotics.CANAnalog.AnalogMode;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANSparkMaxLowLevel.PeriodicFrame;
+import com.revrobotics.ControlType;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.util.Units;
 import frc.robot.Constants;
+import frc.utility.LazyCANSparkMax;
 import frc.utility.NavXMPX_Gyro;
 import frc.utility.OrangeUtility;
 import frc.utility.control.RateLimiter;
@@ -10,32 +27,6 @@ import frc.utility.control.SynchronousPid;
 import frc.utility.control.motion.Path;
 import frc.utility.control.motion.PurePursuitController;
 import frc.utility.math.Rotation2D;
-
-import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import javax.sound.midi.SysexMessage;
-
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-
-import com.revrobotics.*;
-import com.revrobotics.CANAnalog.AnalogMode;
-import com.revrobotics.CANSparkMax.IdleMode;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
-import com.revrobotics.CANSparkMaxLowLevel.PeriodicFrame;
-
-import org.opencv.core.Mat;
-
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.controller.PIDController;
-import edu.wpi.first.wpilibj.SPI;
-import edu.wpi.first.wpilibj.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
-import edu.wpi.first.wpilibj.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj.util.Units;
-import frc.utility.LazyCANSparkMax;
 
 
 public class Drive extends Subsystem {
@@ -239,6 +230,16 @@ public class Drive extends Subsystem {
 
 	synchronized public void setTeleop() {
 		driveState = DriveState.TELEOP;
+	}
+
+	synchronized public SwerveModuleState[] getSwerveModuleStates(){
+		SwerveModuleState[] swerveModuleState = new SwerveModuleState[4];
+		for(int i = 0; i<4; i++){
+			//TODO finish
+			SwerveModuleState moduleState = new SwerveModuleState(1d, Rotation2d.fromDegrees(swerveEncoders[i].getPosition()));
+			swerveModuleState[i] = moduleState;
+		}
+		return swerveModuleState;
 	}
 
 	
--- src/main/java/frc/subsystem/RobotTracker.java
+++ src/main/java/frc/subsystem/RobotTracker.java
@@ -7,8 +7,6 @@ import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.kinematics.SwerveDriveOdometry;
 import frc.robot.Constants;
-import frc.utility.CircularQueue;
-import frc.utility.math.InterpolablePair;
 import frc.utility.math.RigidTransform2D;
 import frc.utility.math.Rotation2D;
 import frc.utility.math.Translation2D;
@@ -32,26 +30,46 @@ public class RobotTracker extends Subsystem {
 		startTime = Timer.getFPGATimestamp();
 	}
 
+
+	@Deprecated
+	/**
+	 * 
+	 * @param time Does not do anything
+	 * @return current rotaion
+	 */
 	synchronized public Rotation2D getGyroAngle(long time) {
-		return null;
+		return Rotation2D.fromWPIRotation2d(swerveDriveOdometry.getPoseMeters().getRotation());
 		
 	}
 
 	synchronized public RigidTransform2D getOdometry() {
-		return null;
-	}
-
-	synchronized public void resetOdometry() {
-		
+		return new RigidTransform2D(Translation2D.fromWPITranslation2d(swerveDriveOdometry.getPoseMeters().getTranslation()), 
+			Rotation2D.fromWPIRotation2d(swerveDriveOdometry.getPoseMeters().getRotation()));
 	}
 
 	/**
-	 * Integrates the encoders and gyro to figure out robot position. A constant
-	 * curvature is assumed
+	 * Resets the position on the feild to 0,0 with a rotaion of 0 degrees
 	 */
+	synchronized public void resetOdometry() {
+		swerveDriveOdometry.resetPosition(new Pose2d(), Rotation2d.fromDegrees(drive.getAngle()));
+	}
+
+  	/**
+   	* Updates the robot's position on the field using forward kinematics and integration of the pose
+   	* over time. This method automatically calculates the current time to calculate period
+   	* (difference between two timestamps). The period is used to calculate the change in distance
+   	* from a velocity. This also takes in an angle parameter which is used instead of the angular
+   	* rate that is calculated from forward kinematics.
+   	*
+   	* @param gyroAngle The angle reported by the gyroscope.
+   	* @param moduleStates The current state of all swerve modules. Please provide the states in the
+   	*     same order in which you instantiated your SwerveDriveKinematics.
+   	* @return The new pose of the robot.
+   	*/
 	@Override
 	public void update() {
-
+		swerveDriveOdometry.update(Rotation2d.fromDegrees(drive.getAngle()), drive.getSwerveModuleStates());
+	
 	}
 
 	/**
@@ -60,7 +78,7 @@ public class RobotTracker extends Subsystem {
 	 */
 	@Deprecated
 	synchronized public void setInitialRotation(Rotation2D offset) {
-		
+		swerveDriveOdometry.resetPosition(new Pose2d(swerveDriveOdometry.getPoseMeters().getTranslation(), offset.getWPIRotation2d()), Rotation2d.fromDegrees(drive.getAngle()));
 	}
 
 	
@@ -71,7 +89,7 @@ public class RobotTracker extends Subsystem {
 	@Deprecated
 	synchronized public void setInitialTranslation(Translation2D offset) {
 		
-		resetOdometry();
+		swerveDriveOdometry.resetPosition(new Pose2d(offset.getWPITranslation2d(), swerveDriveOdometry.getPoseMeters().getRotation()), Rotation2d.fromDegrees(drive.getAngle()));
 	}
 
 	/**
@@ -81,13 +99,17 @@ public class RobotTracker extends Subsystem {
 	 * @param pose The position on the field that your robot is at.
 	 * @param gyroAngle The position on the field that your robot is at.
 	 */
-	synchronized public void resetPosition​(Pose2d pose, Rotation2d gyroAngle){
+	synchronized public void resetPosition(Pose2d pose, Rotation2d gyroAngle){
 		swerveDriveOdometry.resetPosition(pose, gyroAngle);
 	}
 
-
+	/**
+   	* Returns the position of the robot on the field.
+   	*
+   	* @return The pose of the robot (x and y are in meters).
+   	*/
 	synchronized public Pose2d getPoseMeters(){
-		return null;
+		return swerveDriveOdometry.getPoseMeters();
 	}
 
 	@Override
