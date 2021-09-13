a4d1519860a62b6207f2aa3da01db57fb29c7628
varun7654 <59785640+varun7654@users.noreply.github.com>
2021-09-13 09:02:51 -0700

    Revert "code for rotations at the field"
    This reverts commit 98985bbcfd565887ac5c16e782f8777103c9af61.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -219,8 +219,7 @@ public class Robot extends TimedRobot {
 
 		if(autoChooser.getSelected().equals("3 Ball")) option = new ShootOnly(startX);
 		else if(autoChooser.getSelected().equals("3 Ball Drive")) option = new ShootAndMove(startX);
-		
-		option = new AutonomousRotations();
+
 		auto = new Thread(option);
 	
 		auto.start();
